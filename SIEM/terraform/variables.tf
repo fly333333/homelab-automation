f9a0c7e13ea10dd64e0aed1cabd5f9f6071a92f3
fly333333 <matt.ryn.martin@gmail.com>
2026-08-20 17:34:05 -0700

    smol patch

--- SIEM/terraform/variables.tf
+++ SIEM/terraform/variables.tf
@@ -1,30 +1,30 @@
-resource "endpoint" {
+variable "endpoint" {
     type = string
 }
 
-resource "api_token" {
+variable "api_token" {
     type = string
     secret = true
     ephemeral = true
 }
 
-resource "node" {
+variable "node" {
     type = string
 }
 
-resource "vm_ip" {
+variable "vm_ip" {
     type = string
 }
 
-resource "vm_gateway" {
+variable "vm_gateway" {
     type = string
 }
 
-resource "dns_servers" {
+variable "dns_servers" {
     type = list
 }
 
-resource "ssh_public_key" {
+variable "ssh_public_key" {
     type = string
 }
 
