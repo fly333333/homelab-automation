8e7fa326ff859ac362c88bfad78671e43c9c9c1a
fly333333 <matt.ryn.martin@gmail.com>
2026-08-27 16:56:28 -0700

    added the ansible commands to the script for testing

--- SIEM/scripts/run.sh
+++ SIEM/scripts/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# terraform
 cd ../terraform
 terraform init --upgrade
 terraform plan
-# terraform apply
+terraform apply
+
+# ansible
+cd ../ansible
+PUBLIC_IP=192.168.0.15 # IP is hardcoded in terraform. Can change later.
+ansible-playbook --check OS_setup.yml
+ansible-playbook --check OS_hardening.yml
