55fb620b461984930a961c3c1088982cde577014
fly333333 <matt.ryn.martin@gmail.com>
2026-09-02 15:19:14 -0700

    partially done with Elastic/Kibana install script. See TODOs in file

--- SIEM/scripts/run.sh
+++ SIEM/scripts/run.sh
@@ -7,6 +7,9 @@ terraform plan
 terraform apply
 
 # ansible
+echo "CHANGE TO PASSWORD" > ~=/.ansible_vault_pass
+chmod 600 ~/.ansible_vault_pass
 cd ../ansible
 ansible-playbook -i inventory OS_setup.yml
 ansible-playbook -i inventory OS_hardening.yml
+ansible-playbook -i inventory EK_install.yml --vault-password-file ~=/.ansible_vault_pass
