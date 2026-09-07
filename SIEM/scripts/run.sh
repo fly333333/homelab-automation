b495f30c5ad0ae9c3b6e0e5ecbcc253d83731624
fly333333 <matt.ryn.martin@gmail.com>
2026-09-07 14:13:22 -0700

    bash script improvements

--- SIEM/scripts/run.sh
+++ SIEM/scripts/run.sh
@@ -9,8 +9,16 @@
 # ansible
 # echo "CHANGE TO PASSWORD" > ~=/.ansible_vault_pass
 # chmod 600 ~/.ansible_vault_pass
+
+# clear out the vault, so the fresh passwords can be added.
+true > group_vars/siem/vault.yml
 cd ../ansible
 ansible-playbook -i inventory OS_setup.yml
 ansible-playbook -i inventory OS_hardening.yml
 ansible-playbook -i inventory EK_install.yml --vault-password-file ~/.ansible_vault_pass
 ansible-playbook -i inventory fleet_server.yml --vault-password-file ~/.ansible_vault_pass
+
+
+# TODO:
+# - Input for SIEM IP. Copies to vars.yml
+# - Option to output elastic credentials to screen after SIEM deploy.
