7ec7d627df8e38c4e63d6791a79fcf9cc95a646c
fly333333 <matt.ryn.martin@gmail.com>
2026-09-06 19:41:42 -0700

    completed fleet server setup, cleaning up and testing full siem build

--- SIEM/scripts/run.sh
+++ SIEM/scripts/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # terraform
-cd ../terraform
-terraform init --upgrade
-terraform plan
-terraform apply
+# cd ../terraform
+# terraform init --upgrade
+# terraform plan
+# terraform apply
 
 # ansible
-echo "CHANGE TO PASSWORD" > ~=/.ansible_vault_pass
-chmod 600 ~/.ansible_vault_pass
+# echo "CHANGE TO PASSWORD" > ~=/.ansible_vault_pass
+# chmod 600 ~/.ansible_vault_pass
 cd ../ansible
 ansible-playbook -i inventory OS_setup.yml
 ansible-playbook -i inventory OS_hardening.yml
 ansible-playbook -i inventory EK_install.yml --vault-password-file ~=/.ansible_vault_pass
+ansible-playbook -i inventory fleet_server.yml --vault-password-file ~=/.ansible_vault_pass
