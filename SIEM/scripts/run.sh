91593ec202baa85b13e8b3035e36f15babbccfe3
fly333333 <matt.ryn.martin@gmail.com>
2026-09-07 15:45:36 -0700

    more debugging

--- SIEM/scripts/run.sh
+++ SIEM/scripts/run.sh
@@ -13,7 +13,7 @@
 # clear out the vault, so the fresh passwords can be added.
 cd ../ansible
 true > group_vars/siem/vault.yml
-ansible-playbook -i inventory OS_setup.yml
+# ansible-playbook -i inventory OS_setup.yml
 ansible-playbook -i inventory OS_hardening.yml
 ansible-playbook -i inventory EK_install.yml --vault-password-file ~/.ansible_vault_pass
 ansible-playbook -i inventory fleet_server.yml --vault-password-file ~/.ansible_vault_pass
