a75442c4bc48e769c4bdd786b68edab076c8e907
fly333333 <matt.ryn.martin@gmail.com>
2026-09-07 16:06:30 -0700

    more debugging

--- SIEM/scripts/run.sh
+++ SIEM/scripts/run.sh
@@ -14,7 +14,7 @@
 cd ../ansible
 true > group_vars/siem/vault.yml
 # ansible-playbook -i inventory OS_setup.yml
-ansible-playbook -i inventory OS_hardening.yml
+# ansible-playbook -i inventory OS_hardening.yml
 ansible-playbook -i inventory EK_install.yml --vault-password-file ~/.ansible_vault_pass
 ansible-playbook -i inventory fleet_server.yml --vault-password-file ~/.ansible_vault_pass
 
